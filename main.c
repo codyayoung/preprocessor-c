ca0efdc017b8060473636fb0925d8cdc823db9e6
Cody Young <cyoung1623@gmail.com>
2016-04-26 11:28:21 -0700

    Comments
    Cleaned up comments

--- main.c
+++ main.c
@@ -23,13 +23,13 @@ int main(void) {
 		exit(1);
 	}
 
-	p = (char*)malloc(sizeof(char));				// allocate 1 byte to start with
+	p = (char*)malloc(sizeof(char));					// Allocates 1 byte to char array to start
 
 	while ((ch = fgetc(finput)) != EOF)
 	{
-		p[i++] = ch;								// store first byte
-		p = (char*)realloc(p, (i + 1)*sizeof(char));	// allocate room for next byte (characer) by resizing the heap buffer p
-	}
+		p[i++] = ch;									// Store first byte
+		p = (char*)realloc(p, (i + 1)*sizeof(char));	// Allocate room for next byte (character) by resizing heap buffer p
+	} 
 
 	p[i] = '\0';
 	printf("%s\n", p);
