a8ce7c45cc7a1d9d2802c535ee06488b91a2d39f
Cody Young <cyoung1623@gmail.com>
2016-05-03 00:43:08 -0700

    Comment Removal
    Added comment removal functionality. Currently doesn't work on single
    line comments - need to fix single forward slash bug on multilines

--- main.c
+++ main.c
@@ -4,14 +4,20 @@
 //Cody Young
 
 #include <stdio.h>
-#include <stdlib.h.>
+#include <stdlib.h>
+#include <string.h>
 
 FILE *finput, *foutput;
 
+//void replaceInclude(char *p);
+
 int main(void) {
-	char *p;
-	int i = 0;
-	char ch;
+	char *p;				//Pointer to character array - allocates memory
+	char ch;				//Input variable
+	int i = 0;				//Array looping variable
+	int q = 0;				//Used to match char array and check for multiline comments
+	int k = 0;				//Used in remove comment loop
+	
 
 	if (!(finput = fopen("test.c", "r"))) {
 		printf("Error: Cannot process test file. ABORT");
@@ -27,14 +33,59 @@ int main(void) {
 
 	while ((ch = fgetc(finput)) != EOF)
 	{
-		p[i++] = ch;									// Store first byte
-		p = (char*)realloc(p, (i + 1)*sizeof(char));	// Allocate room for next byte (character) by resizing heap buffer p
-	} 
+		p[i++] = ch;									// Store first byte in char array, then increment i
+		p = (char*)realloc(p, (i + 1) * sizeof(char));	// Allocate room for next byte (character) by resizing heap buffer p
+
+		{
+			if (ch == '\'')            //Check for multiline comments
+			{
+				 q = ch;
+				do
+				{
+					putchar(ch);
+					if (ch == '\\') {
+						putchar(fgetc(finput));
+						ch = fgetc(finput);
+					}
+				} while (ch != q);
+				putchar(ch);
+			}
+			else if (ch == '/')              // Check for opening comment
+			{
+				ch = fgetc(finput);
+				if (ch != '*')                  // If not, undo fgetc
+				{
+					putchar(' ');
+					ungetc(ch, finput);
+				}
+				else
+				{
+					putchar(' ');               //Replace comment with whitespace
+					do
+					{
+						k = ch;
+						ch = fgetc(finput);
+					} while (ch != '/' || k != '*');
+				}
+			}
+			else
+			{
+				putchar(ch);
+			}
+		}
+
+	}
 
 	p[i] = '\0';
-	printf("%s\n", p);
+	fprintf(foutput, "%s\n", p); //Printing residual forward slashes for some reason - need to fix
 	free(p);
+
 	fclose(finput);
+	fclose(foutput);
 
 	return 0;
 }
+
+void replaceInclude(char *p) {
+	
+} 
