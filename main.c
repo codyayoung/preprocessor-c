5680de4a51655dfb3da75b374b66c35b6c28abf5
Cody Young <cyoung1623@gmail.com>
2016-05-04 00:03:03 -0700

    Remove comment fixing
    Fixes to remove comment function.

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 //Palomar ID: 008372648
 //Cody Young
 
+#include "header.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 FILE *finput, *foutput;
 
-//void replaceInclude(char *p);
+void replaceInclude(char *p);
 
 int main(void) {
 	char *p;				//Pointer to character array - allocates memory
-	char ch;				//Input variable
+	char ch;				//Input variable from file
 	int i = 0;				//Array looping variable
 	int q = 0;				//Used to match char array and check for multiline comments
 	int k = 0;				//Used in remove comment loop
-	
-
 	if (!(finput = fopen("test.c", "r"))) {
 		printf("Error: Cannot process test file. ABORT");
 		exit(1);
@@ -29,40 +29,42 @@ int main(void) {
 		exit(1);
 	}
 
-	p = (char*)malloc(sizeof(char));					// Allocates 1 byte to char array to start
+	p = (char*)malloc(sizeof(char));										// Allocates 1 byte to char array to start
 
 	while ((ch = fgetc(finput)) != EOF)
 	{
-		p[i++] = ch;									// Store first byte in char array, then increment i
-		p = (char*)realloc(p, (i + 1) * sizeof(char));	// Allocate room for next byte (character) by resizing heap buffer p
-
+		p[i++] = ch;														// Store first byte in char array, then increment i
+		p = (char*)realloc(p, (i + 1) * sizeof(char));						// Allocate room for next byte (character) by resizing heap buffer p
 		{
-			if (ch == '\'')            //Check for multiline comments
+			if (ch == '\'')            //Check for escape comments
 			{
-				 q = ch;
+				q = ch;
 				do
 				{
 					putchar(ch);
-		
+
 				} while (ch != q);
 				putchar(ch);
 			}
-			else if (ch == '/')              // Check for opening comment
+			else if (ch == '/')					// Check for opening comment
 			{
 				ch = fgetc(finput);
-				if (ch != '*')                  // If not, undo fgetc
+				if (ch != '*')                  // If not multiline, move file pointer back
 				{
 					putchar(' ');
 					fseek(finput, -1, SEEK_CUR);
 				}
 				else
 				{
-					putchar(' ');               //Replace comment with whitespace
-					do
-					{
-						k = ch;
-						ch = fgetc(finput);
-					} while (ch != '/' || k != '*');
+					if (ch != '/') {				//Read until end of line - replace comment with whitespace(need to fix)
+						fgetc(finput);
+						putchar(' ');               //Replace comment with whitespace
+						do
+						{
+							k = ch;
+							ch = fgetc(finput);
+						} while (ch != '/' || k != '*');
+					}
 				}
 			}
 			else
@@ -71,11 +73,10 @@ int main(void) {
 				fputc(ch, foutput);
 			}
 		}
-
 	}
-
+		
 	p[i] = '\0';
-	
+
 	free(p);
 
 	fclose(finput);
@@ -85,5 +86,6 @@ int main(void) {
 }
 
 void replaceInclude(char *p) {
-	
-} 
+
+}
+
